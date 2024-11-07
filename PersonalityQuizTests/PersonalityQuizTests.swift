dd5be4d576133f615228094126c5c562661e805d
stefanocasafranca <scasafrancal01@gmail.com>
2024-11-06 18:09:18 -0600

    Initial Commit

---
+++ PersonalityQuizTests/PersonalityQuizTests.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalityQuizTests.swift
+//  PersonalityQuizTests
+//
+//  Created by Stefano Casafranca Laos on 11/6/24.
+//
+
+import Testing
+@testable import PersonalityQuiz
+
+struct PersonalityQuizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
